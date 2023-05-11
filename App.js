7eeeb72f63efbce870a4b334c144eaa37232b7e2
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-05-11 21:26:58 +0300

    passwor/button

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {
   StyleSheet,
-  Text,
   View,
   ImageBackground,
   TextInput,
+  Text,
+  Button,
 } from "react-native";
 
 export default function App() {
@@ -14,7 +15,21 @@ export default function App() {
         style={styles.image}
         source={require("./assets/images/stars-on-night.jpg")}
       >
-        <TextInput style={styles.input} textAlign={"center"} />
+        <View style={styles.form}>
+          <View>
+            <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
+          <TextInput style={styles.input} textAlign={"center"} />
+          </View>
+          <View style={{marginTop: 20}}>
+            <Text style={styles.inputTitle}>PASSWORD</Text>
+            <TextInput 
+            style={styles.input} 
+            textAlign={"center"} 
+              secureTextEntry={true}
+            />
+          </View>
+          <Button title='SING IN'/>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -28,15 +43,23 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: "cover",
-    justifyContent: "center",
-    // alignItems: "center",
+    justifyContent: "center", 
   },
   input: {
-    borderWidth: 1,
-    borderColor: "#f0f8ff",
+    borderWidth: 2,
+    borderColor: "#deb887",
     height: 40,
     borderRadius: 6,
-    marginHorizontal: 40,
+    // marginHorizontal: 40,
     color: "#f0f8ff",
+    fontSize: 20,
+  },
+  form: {
+    marginHorizontal: 40,
+  },
+  inputTitle: {
+    color: "#fff",
+    marginBottom: 10,
+    fontSize: 18,
   },
 });
