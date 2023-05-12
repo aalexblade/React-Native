386582a66ff09f43310a229780e210e8f4c9f156
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-05-13 00:10:08 +0300

    keybordCloseSubmit

--- App.js
+++ App.js
@@ -1,42 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
+  Text,
   View,
   ImageBackground,
   TextInput,
-  Text,
   TouchableOpacity,
   Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableNativeFeedback,
 } from "react-native";
 
 export default function App() {
   console.log(Platform.OS);
-  
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
   return (
+  <TouchableNativeFeedback onPress={keyboardHide}>
     <View style={styles.container}>
       <ImageBackground
         style={styles.image}
         source={require("./assets/images/stars-on-night.jpg")}
       >
-        <View style={styles.form}>
-          <View>
-            <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
-          <TextInput style={styles.input} textAlign={"center"} />
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <View
+            style={{ ...styles.form, marginBottom: isShowKeyboard ? 20 : 100 }}
+          >
+            <View style={styles.header}>
+              <Text style={styles.headerTitle}>Hello World</Text>
+              <Text style={styles.headerTitle}>Welcome back</Text>
+            </View>
+            <View>
+              <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+            </View>
+            <View style={{ marginTop: 20 }}>
+              <Text style={styles.inputTitle}>PASSWORD</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                secureTextEntry={true}
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+            </View>
+            <TouchableOpacity
+              activeOpacity={0.8}
+              style={styles.btn}
+              onPress={keyboardHide}
+            >
+              <Text style={styles.btnTitle}>SIGN IN</Text>
+            </TouchableOpacity>
           </View>
-          <View style={{marginTop: 20}}>
-            <Text style={styles.inputTitle}>PASSWORD</Text>
-            <TextInput 
-            style={styles.input} 
-            textAlign={"center"} 
-              secureTextEntry={true}
-            />
-          </View>
-          <TouchableOpacity activeOpacity={0.5} style={styles.btn}>
-            <Text style={styles.btnTitle}>SING IN</Text>
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
+        </KeyboardAvoidingView>
+        </ImageBackground>
     </View>
+   </TouchableNativeFeedback>
   );
 }
 
@@ -48,39 +78,55 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: "cover",
-    justifyContent: "center", 
+    // justifyContent: "flex-end",
+    justifyContent: "center",
+    // alignItems: "center",
   },
   input: {
-    borderWidth: 2,
-    borderColor: "#deb887",
+    borderWidth: 1,
+    borderColor: "#f0f8ff",
     height: 40,
     borderRadius: 6,
-    // marginHorizontal: 40,
+
     color: "#f0f8ff",
-    fontSize: 20,
   },
   form: {
     marginHorizontal: 40,
   },
   inputTitle: {
-    color: "#fff",
+    color: "#f0f8ff",
     marginBottom: 10,
     fontSize: 18,
   },
-
   btn: {
-    backgroundColor: Platform.OS === "ios" ? "transporter" : "#1e90ff",
-    height: 40,
     borderRadius: 6,
-    marginTop: 40,
     borderWidth: 1,
-    borderColor: Platform.OS === "ios" ? "#f0f8ff" : "transporter",
+    height: 40,
+    marginTop: 40,
     justifyContent: "center",
     alignItems: "center",
-    marginHorizontal: 80,
+    marginHorizontal: 20,
+    ...Platform.select({
+      ios: {
+        backgroundColor: "transparent",
+        borderColor: "#f0f8ff",
+      },
+      android: {
+        backgroundColor: "#4169e1",
+        borderColor: "transparent",
+      },
+    }),
   },
   btnTitle: {
-    color: Platform.OS === "ios" ? "#4169e1" : "#fff",
+    color: Platform.OS === "ios" ? "#4169e1" : "#f0f8ff",
     fontSize: 18,
+  },
+  header: {
+    alignItems: "center",
+    marginBottom: 150,
+  },
+  headerTitle: {
+    fontSize: 30,
+    color: "#fff",
   }
 });
